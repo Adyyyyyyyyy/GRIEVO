aabd7a3bbfad2955e0b87c86d4f1296c4fadd588
Adyyyyyyyyy <144515937+Adyyyyyyyyy@users.noreply.github.com>
2026-01-28 12:39:44 +0530

    Registered admin and resolver routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from app.routes.auth_routes import auth_bp
 from app.routes.grievance_routes import grievance_bp
+from app.routes.admin_routes import admin_bp
+from app.routes.resolver_routes import resolver_bp
 
 def create_app():
     app = Flask(__name__)
@@ -8,6 +10,9 @@ def create_app():
     # Register blueprints
     app.register_blueprint(auth_bp)
     app.register_blueprint(grievance_bp)
+    app.register_blueprint(admin_bp)
+    app.register_blueprint(resolver_bp)
+
 
     @app.route("/")
     def home():
